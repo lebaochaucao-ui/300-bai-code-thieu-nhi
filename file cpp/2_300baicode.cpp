653228e535dc9e4fb4401065fa6585582b0eee9c
Natsuki <lebaochaucao@gmail.com>
2026-09-16 19:55:17 +0700

    Add files via upload

---
+++ file cpp/2_300baicode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a;
+    if(a > 1000000000){
+        cout << "ko hop le! \n";
+        return 0;
+
+    }
+    b = a - 2;
+    cout << b;
+
+
+}
